d9b72c4bd96a3f1635b4cc233b01c70b2ed35b08
robotictacos <dave@codeandbeer.net>
2024-08-25 17:28:05 -0400

    chore: Add final steps script and update systemd services

---
+++ files/scripts/final-steps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -oue pipefail 
+git clone https://github.com/pop-os/gnome-shell-extension-system76-power.git
+cd gnome-shell-extension-system76-power
+dnf install nodejs-typescript
+make
+make install
+
+mkdir -p /mnt/backup
+mkdir -p /mnt/vm
+mkdir -p /mnt/games
