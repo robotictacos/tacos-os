e29682ea68bfe133d3d7b19165785b54e8d3f85b
robotictacos <dave@codeandbeer.net>
2024-08-26 12:51:23 -0400

    Update final-steps.sh script to install system76 gnome widget

--- files/scripts/final-steps.sh
+++ files/scripts/final-steps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -oue pipefail
 # install system76 gnome widget
+dnf install nodejs
 npm install -g typescript
 git clone https://github.com/pop-os/gnome-shell-extension-system76-power.git
 cd gnome-shell-extension-system76-power
-dnf install nodejs-typescript
 make
 make install
+cp -r /root/.local/share/gnome-shell/extensions/system76-power@system76.com/ /usr/share/gnome-shell/extensions/
+
 # dnf swap ffmpeg-free ffmpeg --allowerasing
 # dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 # dnf update @sound-and-video
