2970b06f1d7f1e23ecbacc4acb2b4655a7eec9ff
robotictacos <dave@codeandbeer.net>
2024-08-25 22:26:47 -0400

    chore: Update multimedia and sound-and-video packages
    This commit updates the multimedia and sound-and-video packages in the final-steps.sh script. It swaps the ffmpeg-free package with ffmpeg and updates the @multimedia and @sound-and-video package groups. Additionally, it swaps the mesa-va-drivers package with mesa-va-drivers-freeworld and mesa-vdpau-drivers with mesa-vdpau-drivers-freeworld. These changes aim to improve multimedia support and ensure the latest versions of the packages are installed.

--- files/scripts/final-steps.sh
+++ files/scripts/final-steps.sh
@@ -5,6 +5,12 @@ cd gnome-shell-extension-system76-power
 dnf install nodejs-typescript
 make
 make install
+dnf swap ffmpeg-free ffmpeg --allowerasing
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf update @sound-and-video
+dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+
 
 # mkdir -p /mnt/backup
 # mkdir -p /mnt/vm
