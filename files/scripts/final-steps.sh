65a2aad4acc61d3e226ce83e1e0b2c1471fcc900
robotictacos <dave@codeandbeer.net>
2024-08-26 07:23:53 -0400

    chore: Update multimedia and sound-and-video packages

--- files/scripts/final-steps.sh
+++ files/scripts/final-steps.sh
@@ -5,11 +5,11 @@ cd gnome-shell-extension-system76-power
 dnf install nodejs-typescript
 make
 make install
-dnf swap ffmpeg-free ffmpeg --allowerasing
-dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf update @sound-and-video
-dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+# dnf swap ffmpeg-free ffmpeg --allowerasing
+# dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+# dnf update @sound-and-video
+# dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+# dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 
 
 # mkdir -p /mnt/backup
