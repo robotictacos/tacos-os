01b334885b7804d130bfa2f47991dcb8614fd382
robotictacos <dave@codeandbeer.net>
2024-08-27 06:11:14 -0400

    chore: Update enable-system76-power.sh and related recipes
    This commit updates the enable-system76-power.sh script and related recipes to install the system76-dkms, system76-power, system76-driver, system76-firmware, firmware-manager, system76-io-dkms, and system76-acpi-dkms packages using dnf. The previous code used the incorrect variable name for the kernel version, which has been fixed in this commit. These changes aim to improve the functionality and maintainability of the codebase.

--- files/scripts/enable-system76-power.sh
+++ files/scripts/enable-system76-power.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-KERNEL_VERSION=$(uname -r)
-rpm-ostree install kernel-devel-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} -y
+# KERNEL_VERSION=$(uname -r)
+# rpm-ostree install kernel-devel-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} -y
+
+bash ./remove-fedora-repos.sh
+dnf install system76-dkms system76-power system76-driver system76-firmware firmware-manager system76-io-dkms system76-acpi-dkms
+systemctl enable com.system76.PowerDaemon.service system76-power-wake system76-firmware-daemon --now
+systemctl enable --user com.system76.FirmwareManager.Notify.timer
+system76-power graphics integrated
 
